3abe5e0a9feeae9db818cc5909d4b61f897e9e4c
iklimduman <iklimdumann@gmail.com>
2021-12-04 22:38:18 +0300

    add navbar and logo

---
+++ ecommerce/src/Components/Navbar.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Search, ShoppingCart, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from '@material-ui/core';
+import "./Navbar.css";
+
+const Container = styled.div`
+    height : 60px ;
+`
+const Wrapper = styled.div`
+    padding : 10px 20px ;
+    display : flex ;
+    justify-content : space-between ;
+`
+const Left = styled.div`
+flex : 1;
+display : flex ;
+`
+
+const Right = styled.div`
+flex : 1;
+display : flex ;
+align-items : center ;
+justify-content : flex-end ;
+padding-right : 25px ;
+`
+
+const Center = styled.div`
+    align-items: center;
+    align-self: center;
+    justify-content: center;
+    display : flex ;
+    flex : 1
+`
+const MenuItem = styled.div`
+    cursor : pointer ;
+    margin-left : 25px ;
+`
+
+const SearchContainer = styled.div`
+    align-self: center;
+    border : 1px solid lightgray ;
+    display : flex ;
+    width : 70% ;
+    align-items : center ;
+    padding : 5px ;
+    border-radius : 25px ;
+    `
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <div className="logo-container">
+
+                    </div>
+                </Left>
+                <Center>
+                    <SearchContainer>
+                        <Search style={{color:"gray"}}/>
+                    </SearchContainer>
+                </Center>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign in</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Navbar
