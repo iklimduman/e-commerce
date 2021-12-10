016eace74240a9dbc4121998d4e844fb92efde0a
iklimduman <iklimdumann@gmail.com>
2021-12-10 23:13:44 +0300

    Add slider functional components

--- ecommerce/src/Components/Slider.jsx
+++ ecommerce/src/Components/Slider.jsx
@@ -3,17 +3,22 @@ import styled from 'styled-components';
 import { ArrowLeftOutlined } from '@material-ui/icons';
 import { ArrowRightOutlined } from '@material-ui/icons';
 import myImage from "../Assets/slide-1.jpg";
+import { sliderItems } from "../Data";
+import { useState } from 'react';
 
 
 const Container = styled.div`
-    margin-top : 2vh ;
-    width : 100% ;
-    height : 98vh ;
-    display : flex ;
-    position : relative ;
+margin-top : 2vh ;
+    width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
 `
 
 const Arrow = styled.div`
+    position : absolute ;
+    z-index : 100 ;
     width : 50px ;
     height : 50px ;
     border-radius : 50% ;
@@ -33,67 +38,122 @@ const Arrow = styled.div`
 `
 
 const Wrapper = styled.div`
-    width : 100% ;
-    height : 100% ;
-    background-image : url(${myImage});
-    background-repeat : no-repeat ;
-    background-size : cover ;
-    display : flex ;
+    height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${(props) => props.slideIndex * -100}vw);
+
+    
 `
 
 const Slide = styled.div`
-    display : flex ;
-    align-items : center ;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    
+    background-color: #${(props) => props.bg};
+    
+   
+    align-items: center;
 `
 
 const InfoContainer = styled.div`
     flex : 1;
+    width : 25vw ;
+    height : 50vh ;
     display : flex ;
     align-items : center ;
     justify-content : center ;
 `
 
 const Info = styled.div`
-    width : 70% ;
-    height : 70% ;
+    width : 35vw ;
+    margin-left : 10vw ;
+    height : 50vh ;
     font-family: 'Bebas Neue', cursive;
     font-size : 100px ;
-    line-height : 5px ;
+    line-height : 105px ;
     padding : 50px  ;
     text-align : right ;
     background-color : rgb(249,196,255,0.5) ;
     position : relative ;
+    color : rgb(255,255,255);
+    box-shadow: 1px 1px 0px #$({props => props.boxshadow}),
+                2px 2px 0px #$({props => props.boxshadow}),
+                3px 3px 0px #$({props => props.boxshadow}),
+                4px 4px 0px #$({props => props.boxshadow}),
+                5px 5px 0px #$({props => props.boxshadow}),
+                6px 6px 0px #$({props => props.boxshadow}),
+}
 `
 
 
-const ImageContainer = styled.div`
+const ImgContainer = styled.div`
     flex : 1 ;
 `
 
 
 const Slider = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+        }
+    };
+
     return (
         <Container>
-            <Arrow direction="left">
+            <Arrow direction="left" onClick={() => handleClick("left")}>
                 <ArrowLeftOutlined />
             </Arrow>
-                <Wrapper>
-                    <InfoContainer>
-                        <Info>
-                            <p style={{ color: "#FEF9FF" }}>New</p>
-                            <p style={{ color: "#FEF9FF" }}>Collection</p>
-                            <p style={{ color: "#FEF9FF" }}>Out</p>
-                            <div style={{ fontSize: "70px", position: "absolute", right: "50px", bottom: "0px", color: "#FEF9FF" }}>
-                                <p>Spring /</p>
-                                <p>Summer</p>
-                                <p>'21</p>
-                            </div>
-                        </Info>
-                    </InfoContainer>
-                    <ImageContainer></ImageContainer>
-                </Wrapper>
 
-            <Arrow direction="right">
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map((item) => (
+                    <Slide bg={item.bg} key={item.id} src={item.src}>
+                        <InfoContainer>
+                            <Info boxshadow={item.bg}>{item.title}</Info>
+
+                            {/*
+                            <Title>{item.title}</Title>
+                            <Desc>{item.desc}</Desc>
+                            <Button>SHOW NOW</Button>
+                            */}
+                        </InfoContainer>
+                        <ImgContainer>
+
+                        </ImgContainer>
+
+                    </Slide>
+                ))}
+            </Wrapper>
+
+            {/*
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map((item) => (
+                    <Slide src={myImage}>
+                        <InfoContainer>
+                            <Info>
+                                <p style={{ color: "#FEF9FF" }}>New</p>
+                                <p style={{ color: "#FEF9FF" }}>Collection</p>
+                                <p style={{ color: "#FEF9FF" }}>Out</p>
+                                <div style={{ fontSize: "70px", position: "absolute", right: "50px", bottom: "0px", color: "#FEF9FF" }}>
+                                    <p>Spring /</p>
+                                    <p>Summer</p>
+                                    <p>'21</p>
+                                </div>
+                            </Info>
+                        </InfoContainer>
+                        <ImageContainer></ImageContainer>
+                    </Slide>
+                ))}
+            </Wrapper>
+            */}
+
+            <Arrow direction="right" onClick={() => handleClick("right")}>
                 <ArrowRightOutlined />
             </Arrow>
 
---
+++ ecommerce/src/Data.js
@@ -0,0 +1,29 @@
+export const sliderItems = [
+    {
+        key: 1,
+        id: 1,
+        src: "../Assets/slide-1.jpg",
+
+        title: "SUMMER SALE!",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "C271D2"
+    },
+    {
+        key: 2,
+        id: 2,
+        src: "../Assets/slide-1.jpg",
+
+        title: "NEW COLLECTION!",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "71D289"
+    },
+    {
+        key: 3,
+        id: 3,
+        src: "../Assets/slide-1.jpg",
+
+        title: "NEW OFFER!",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "AD71D2"
+    }
+]
