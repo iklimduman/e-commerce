906385f8dcbb63957102ae44581f124dd3f2fb52
iklimduman <iklimdumann@gmail.com>
2021-12-11 23:24:17 +0300

    Add title component

--- ecommerce/src/Components/MainProductConteiner.js
+++ ecommerce/src/Components/MainProductConteiner.js
@@ -2,18 +2,31 @@ import { Products } from "../Data";
 import React from "react";
 import styled from "styled-components";
 import MainProduct from "./MainProduct";
+import Title from "./Title";
 
-const Container = styled.div`` ;
+const TitleInput = [
+    {
+        text : "Popular This Month" 
+    }
+    
+]
+
+const Container = styled.div``;
 
 const MainProductContainer = () => {
     return (
-        <Container>
-        {Products.map((item)=>(
-            <MainProduct item={item} key={item.id} />
-        ))}
+        <div>
+        <Title item={TitleInput[0]}/>
+            <Container>
+                {Products.map((item) => (
+                    <MainProduct item={item} key={item.id} />
+                ))}
 
-        </Container>
+            </Container>
+
+        </div>
+
     )
 };
 
-export default MainProductContainer ;
+export default MainProductContainer;
---
+++ ecommerce/src/Components/Title.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding-left : 25px ;
+    text-transform : uppercase ;
+    font-family: 'Nunito Sans', sans-serif;
+`
+
+const Text = styled.h1``
+
+const Title = ({item}) => {
+    return (
+        <Container>
+            <Text>{item.text}</Text>
+        </Container>
+    )
+}
+
+export default Title ;
