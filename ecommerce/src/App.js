c433293b9914d4d2596821e93081492e97338716
iklimduman <iklimdumann@gmail.com>
2022-01-15 23:11:51 +0300

    newsletter add elements and basic style

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Slider />
       <CategoryContainer />
       <CarouselContainer />
+      <Newsletter />
     </div>
   );
 }
--- ecommerce/src/Components/Newsletter.js
+++ ecommerce/src/Components/Newsletter.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color : rgb(232,252,242,0.5) ;
+    padding-top : 5% ;
+    `
+
+const Header = styled.span`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform : uppercase ;
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 2em;
+    line-height: 2.5em;
+    `
+
+const Description = styled.span`
+    display: flex;
+    align-items : center ;
+    justify-content: center ;
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 1.2em;
+    line-height: 2.5em;
+    `
+
+const İnputContainer = styled.div`
+    display: flex;
+    align-items : center ;
+    justify-content: center ;
+    `
+
+const Input = styled.input`
+    width : 30% ;
+`
+
+const Subscribe = styled.button`
+    font-family: 'Nunito Sans', sans-serif;
+    `
 
 const Newsletter = () => {
-    return (
-        <div>
 
-        </div>
+    const inputElement = useRef("")
+
+    return (
+        <Container>
+            <Header>Newsletter</Header>
+            <Description>Subscribe to our newsletter to keep up with our latest news!</Description>
+            <İnputContainer>
+                <Input autoComplete="off" ref={inputElement} type="email">
+
+                </Input>
+                <Subscribe>
+                    SUBSCRIBE
+                </Subscribe>
+            </İnputContainer>
+
+        </Container>
     )
 }
 
-export default Newsletter ;
+export default Newsletter;
