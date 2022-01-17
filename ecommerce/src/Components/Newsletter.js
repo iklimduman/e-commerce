6ebb69d3886f799155cf6aaa567a066c5f86da11
iklimduman <iklimdumann@gmail.com>
2022-01-17 14:04:08 +0300

    newsletter last commit

--- ecommerce/src/Components/Newsletter.js
+++ ecommerce/src/Components/Newsletter.js
@@ -5,6 +5,11 @@ import SendIcon from '@mui/icons-material/Send';
 const Container = styled.div`
     background-color : rgb(232,252,242,0.5) ;
     padding-top : 5% ;
+    height : 40vh ;
+    display : flex ;
+    align-items : center ;
+    justify-content : center ;
+    flex-direction : column ;
     `
 
 const Header = styled.span`
@@ -13,7 +18,8 @@ const Header = styled.span`
     justify-content: center;
     text-transform : uppercase ;
     font-family: 'Nunito Sans', sans-serif;
-    font-size: 2em;
+    font-size: 60px;
+    font-weight : 300 ;
     line-height: 2.5em;
     `
 
@@ -24,20 +30,31 @@ const Description = styled.span`
     font-family: 'Nunito Sans', sans-serif;
     font-size: 1.2em;
     line-height: 2.5em;
+    margin-bottom : 20px ;
     `
 
 const İnputContainer = styled.div`
+    width : 30% ;
     display: flex;
-    align-items : center ;
-    justify-content: center ;
+    justify-content : space-between ;
     `
 
 const Input = styled.input`
     width : 30% ;
+    height : 30px ;
+    flex : 8 ;
+    padding : 0 !important ; 
 `
 
 const Subscribe = styled.button`
     font-family: 'Nunito Sans', sans-serif;
+    flex : 1 ;
+    border : none ;
+    background-color : #3D4780 ;
+    color : white ;
+    display : flex ;
+    align-items : center ;
+    justify-content : center ;
     `
 
 const Newsletter = () => {
@@ -49,7 +66,7 @@ const Newsletter = () => {
             <Header>Newsletter</Header>
             <Description>Subscribe to our newsletter to keep up with our latest news!</Description>
             <İnputContainer>
-                <Input autoComplete="off" ref={inputElement} type="email">
+                <Input autoComplete="off" ref={inputElement} type="email" placeholder="Enter your e-mail">
 
                 </Input>
                 <Subscribe>
