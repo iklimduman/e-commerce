812fb2c25cd5d8c1e5a91e03d42ae230f04892ea
iklimduman <iklimdumann@gmail.com>
2022-01-21 10:18:05 +0300

    create size buttons

--- ecommerce/src/Pages/SingleProduct.js
+++ ecommerce/src/Pages/SingleProduct.js
@@ -9,6 +9,8 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
     width : 70% ;
+    padding-top : 100px ;
+    height : 100vh ;
     margin : auto ;
     display : flex ;
     flex-direction : row ;
@@ -18,7 +20,8 @@ const ImageContainer = styled.div`
     flex : 1 ;
 `
 
-const Image = styled.img``
+const Image = styled.img`
+    height : 70vh ;`
 
 const InfoContainer = styled.div`
     flex  : 1 ;
@@ -27,6 +30,25 @@ const InfoContainer = styled.div`
 const Title = styled.h1``
 
 const Description = styled.p``
+
+const SmallTitle = styled.h3``
+
+const ButtonContainer = styled.div``
+
+const SizeButton = styled.button`
+    width : 32px ;
+    height : 32px ;
+    margin : 0 5px ;
+    text-align : center ;
+`
+
+const BuyContainer = styled.div`
+    margin-top : 50px ;
+`
+
+const Favorite = styled.button``
+
+const CartButton = styled.button``
 
 const SingleProduct = () => {
     return (
@@ -44,6 +66,20 @@ const SingleProduct = () => {
                     <Description>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus quam id leo in.
                     </Description>
+                    <SmallTitle>
+                        Select Size
+                    </SmallTitle>
+                    <ButtonContainer>
+                        <SizeButton style={{borderRadius : "50%"}}>XS</SizeButton>
+                        <SizeButton style={{borderRadius : "100%"}}> S</SizeButton>
+                        <SizeButton style={{borderRadius : "50%"}}> M</SizeButton>
+                        <SizeButton style={{borderRadius : "50%"}}>L</SizeButton>
+                        <SizeButton style={{borderRadius : "50%"}}>XL</SizeButton>
+                    </ButtonContainer>
+                    <BuyContainer>
+                        <Favorite>Wishlist</Favorite>
+                        <CartButton>Add to Cart</CartButton>
+                    </BuyContainer>
                 </InfoContainer>
             </Wrapper>
 
