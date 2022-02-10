e0b80b0f31fef9589d96b2c7cedfce4ab4304197
iklimduman <iklimdumann@gmail.com>
2022-02-10 22:41:57 +0300

    send get request and post send response to post request with postman

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv") ;
+const userRoute = require("./routes/users") ;
 
 dotenv.config() ;
 
@@ -12,6 +13,10 @@ mongoose.connect(process.env.MONGO_URL
 }).catch((e) => {
     console.log(e)
 }) ;
+
+app.use(express.json()) ;
+
+app.use("/api/users" , userRoute) ;
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("back end server is running")
---
+++ ecommerce-api/routes/users.js
@@ -0,0 +1,13 @@
+const router = require("express").Router() ;
+
+router.get("/usertest" , (req,res) =>{
+    res.send("res send succesful")
+} )
+
+router.post("/userposttest" , (req,res) => {
+    const username = req.body.username ;
+    console.log(username) ;
+    res.send("your username is " + username)
+}) ;
+
+module.exports = router
