b6c8053d5161b9ca8aca00c3d067eaefcfd1c9c6
iklimduman <iklimdumann@gmail.com>
2022-02-16 10:26:49 +0300

    Let only admin to find spesific user data except their password by their ID. GET request /find/:id

--- ecommerce-api/routes/users.js
+++ ecommerce-api/routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { update } = require("../Models/User");
-const User = require("../Models/User") ;
-const { verifyToken, verifyTokenAuthorization } = require("./verifyToken");
+const User = require("../Models/User");
+const { verifyToken, verifyTokenAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 /* TEST CODE FOR GET AND POST 
 router.get("/usertest" , (req,res) =>{
@@ -18,37 +18,48 @@ router.post("/userposttest" , (req,res) => {
 // we can register 
 
 router.put("/:id", verifyTokenAuthorization, async (req, res) => {
-   
+
     if (req.body.password) {
-        
+
         req.body.password = CryptoJS.AES.encrypt(
             req.body.password,
             process.env.PASS_SEC
         ).toString()
     }
 
-    try{
-        
+    try {
+
         // default mongoDB function
-        const updatedUser = await User.findByIdAndUpdate(req.params.id , {
-            $set : req.body
-        },{new : true})
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
         res.status(200).json(updatedUser);
     }
-    catch(err){
-        
-        res.status(400).json("Error!") ;
+    catch (err) {
+
+        res.status(400).json("Error!");
     }
 });
 
 // DELETE
-
-router.delete("/:id" , verifyTokenAuthorization , async(req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id) ;
+router.delete("/:id", verifyTokenAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User has been deleted...");
     }
-    catch(err){
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others)
+    }
+    catch (err) {
         res.status(500).json(err);
     }
 })
