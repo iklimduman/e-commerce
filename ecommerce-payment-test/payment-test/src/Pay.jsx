990132d4f1d8783daeef168a215fe2f956eb713b
iklimduman <iklimdumann@gmail.com>
2022-02-18 19:13:27 +0300

    complete payment

--- ecommerce-payment-test/payment-test/src/Pay.jsx
+++ ecommerce-payment-test/payment-test/src/Pay.jsx
@@ -1,31 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import StripeCheckout from 'react-stripe-checkout';
 
 import styled from "styled-components";
+import axios from "axios";
 
 const Container = styled.div``
 
 const PaymentButton = styled.button``
 
-const KEY = "pk_test_51KTnoHGspQcUdK7Etqe4JxmiTS1ykUICfbo5Gj0ScZQKgPCRIQ1n3t0Ne3c9blIhrUU7wtTWU823muOzpr0p5aZu00j28jxe7N" ;
+const KEY = "pk_test_51KTnoHGspQcUdK7Etqe4JxmiTS1ykUICfbo5Gj0ScZQKgPCRIQ1n3t0Ne3c9blIhrUU7wtTWU823muOzpr0p5aZu00j28jxe7N";
 
 
 const Pay = () => {
 
+    const [stripeToken, setStripeToken] = useState(null);
+
+
     const onToken = (token) => {
-        console.log(token)
+        setStripeToken(token);
     }
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                const res = await axios.post("http://localhost:5000/api/checkout/payment",
+                    {
+                        tokenId: stripeToken.id,
+                        amount: 2000,
+                    }
+                );
+                console.log(res.data);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        };
+        if (stripeToken) {
+            makeRequest();
+        }
+
+    }, [stripeToken])
+
+
     return (
         <Container>
-            <StripeCheckout 
-            name='Lotus-Shop'
-            image='./logo.png'
-            billingAddress
-            shippingAddress
-            description='Your total is $20'
-            amount={2000}
-            token={onToken}
-            stripeKey={KEY}>
+            <StripeCheckout
+                name='Lotus-Shop'
+                image='./logo.png'
+                billingAddress
+                shippingAddress
+                description='Your total is $20'
+                amount={2000}
+                token={onToken}
+                stripeKey={KEY}>
                 <PaymentButton>PAY NOW</PaymentButton>
             </StripeCheckout>
 
@@ -33,4 +60,4 @@ const Pay = () => {
     )
 }
 
-export default Pay ;
+export default Pay;
