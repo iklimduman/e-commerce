7d97f96e3b26c751596aeacbd866be8277903888
iklimduman <iklimdumann@gmail.com>
2022-02-21 18:34:11 +0300

    react-router-dom v6 routing

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -8,12 +8,25 @@ import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Cart from './Pages/Cart';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Home />
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/product" element={<SingleProduct/>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
