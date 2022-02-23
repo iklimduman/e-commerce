b3aa526b875e4bf3aae71a4ef6c41a271655151f
iklimduman <iklimdumann@gmail.com>
2022-02-23 17:34:45 +0300

    get request to ecommerce-api server

--- ecommerce/src/Components/ProductsContainer.js
+++ ecommerce/src/Components/ProductsContainer.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { Products } from "../Data";
 import ProductCard from "./ProductCard";
 
+import axios from "axios" ;
+
 const ProductContainer = styled.div`
     margin : auto ;
     width : 90% ;
@@ -17,10 +19,20 @@ const ProductsContainer = ({cat,filters,sort}) => {
     const [products,setProducts] = useState([]) ;
     const [filteredProducts , setFilteredProducts] = useState([]) ;
 
-    useEffect(()=>{
+    // get products from api
 
+    useEffect(()=>{
+        const getProducts = async ()=>{
+            try{
+                const res = await axios.get("http://localhost:5000/api/products") ;
+                console.log(res) ;
+            }
+            catch(err){
+            }
+        }
+        getProducts() ;
     } , [cat]) ;
-    
+
     return (
         <ProductContainer>
                 {Products.map((item) => (
