256d6cd841260186e0d4841c9cc8df7b4a701682
iklimduman <iklimdumann@gmail.com>
2022-02-24 13:10:32 +0300

    add sorting algorithm for products page, oldest to newest , price asc to desc

--- ecommerce/src/Components/ProductsContainer.js
+++ ecommerce/src/Components/ProductsContainer.js
@@ -16,7 +16,10 @@ const ProductContainer = styled.div`
 
 const ProductsContainer = ({ cat, filters, sort }) => {
 
-    console.log("category = " + cat);
+    // filters : { color : ".." , size : ".."} 
+    // cat : woman, sale, casual etc..
+
+    console.log("sort = " + sort)
 
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
@@ -28,11 +31,14 @@ const ProductsContainer = ({ cat, filters, sort }) => {
             try {
                 const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}`
                     : `http://localhost:5000/api/products`);
-                if(res.data.length == 0) {
-                    res = await axios.get(`http://localhost:5000/api/products`) ;
+                if (res.data.length == 0) {
+                    res = await axios.get(`http://localhost:5000/api/products`);
                 }
-                console.log("api response = ") ;
-                console.log(res) ;
+
+                setProducts(res.data);
+                console.log("products = ");
+                console.log(products);
+                console.log(res.data);
             }
             catch (err) {
             }
@@ -43,12 +49,28 @@ const ProductsContainer = ({ cat, filters, sort }) => {
     useEffect(() => {
         cat &&
             setFilteredProducts(
-                products.filter((item) => Object.entries(filters)
-                    .every(([key, value]) => item[key].includes(value)))
-            )
-    }, [products, cat, filters]) ;
+                products.filter((item) => {
+                    return Object.entries(filters)
+                        .every(([key, value]) => item[key].includes(value))
+                })
 
-    console.log("filtered products = " + filteredProducts) ;
+            )
+    }, [products, cat, filters]);
+
+    useEffect(()=> {
+        if(sort === "newest"){
+            setFilteredProducts((prev) => [...prev].sort((a,b) => a.createdAt - b.createdAt))
+        }
+        else if( sort === "price-asc"){
+            setFilteredProducts((prev) => [...prev].sort((a,b) => a.price - b.price))
+        }
+        else{
+            setFilteredProducts((prev) => [...prev].sort((a,b) => b.price - a.price))
+        }
+
+    } , [ sort ])
+
+    console.log("filtered products = " + filteredProducts);
 
     return (
         <ProductContainer>
