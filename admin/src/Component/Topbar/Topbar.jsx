7ce44421853b7c3a5f50a0c2bd90fba58ce8632a
iklimduman <iklimdumann@gmail.com>
2022-03-11 16:12:21 +0300

    finish navbar

--- admin/src/Component/Topbar/Topbar.jsx
+++ admin/src/Component/Topbar/Topbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Topbar.css";
 
-import logo from "../../Images/lotus-logo.jpeg" ;
+import logo from "../../Images/lotus-logo.jpeg";
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language'; 
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const iconStyle = {
+    width: '35px',
+    height: '35px'
+}
 
 const Topbar = () => {
     return (
@@ -10,12 +17,22 @@ const Topbar = () => {
             <div className="topbar-wrapper">
                 <div className="top-left">
                     <div className="logo">
-                        <img class="image" src={logo} alt="Alternative Text"/>
+                        <img class="image" src={logo} alt="Alternative Text" />
                     </div>
                 </div>
                 <div className="top-right">
-                    right
-                    <NotificationsNoneIcon></NotificationsNoneIcon>
+                    <div className="topbarIconContainer">
+                        <NotificationsNoneIcon htmlColor="#5A4AE2" style={iconStyle} />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <LanguageIcon htmlColor="#5A4AE2" style={iconStyle} />
+                        
+                    </div>
+                    <div className="topbarIconContainer">
+                        <SettingsIcon htmlColor="#5A4AE2" style={iconStyle} />
+                    </div>
+                    <div className="avatar" />
                 </div>
             </div>
         </div>
