eb34c30fd522cce3e9140e2ab6291955b6d0ea30
iklimduman <iklimdumann@gmail.com>
2022-03-16 19:46:22 +0300

    add lates transactions, depend on transaction status change button class

--- admin/src/Component/Transactions/Transactions.jsx
+++ admin/src/Component/Transactions/Transactions.jsx
@@ -1,10 +1,63 @@
 import React from "react";
-import "./Transactions.css" ;
+import "./Transactions.css";
+import "../TeamMember/TeamMember.css";
+
+import { latestTransactions } from "../../dummyData";
+
+const Button = ({type}) => {
+    return (
+        <button className={"statusButton " + type}>{type}</button>
+    )
+}
+
+const Transaction = () => {
+    return (
+        <div className="transaction">
+            <table className="transaction-table">
+                <tr className="transaction-tr">
+                    <th className="transaction-th">
+                        Customer
+                    </th>
+                    <th className="transaction-th">
+                        Date
+                    </th>
+                    <th className="transaction-th">
+                        Amount
+                    </th>
+                    <th className="transaction-th">
+                        Status
+                    </th>
+                </tr>
+                {latestTransactions.map((transaction) => {
+                    return (
+                        <tr className="transaction-tr-border">
+                            <td className="user-td">
+                                <img src={transaction.img} className="user-img"/>
+                                <span>{transaction.customer}</span>
+                            </td>
+                            <td className="date-td">
+                                {transaction.date}
+                            </td>
+                            <td className="amount-td">
+                                {transaction.amount}
+                            </td>
+                            <td className="status-td">
+                                <Button type={transaction.status}/>
+                            </td>
+                        </tr>
+                    )
+                })}
+
+            </table>
+        </div>
+    )
+}
 
 const Transactions = () => {
     return (
         <div className="transaction-wrapper">
-            transaction
+            <span className="title">Latest Transactions</span>
+            <Transaction />
         </div>
     )
 }
--- admin/src/dummyData.js
+++ admin/src/dummyData.js
@@ -71,3 +71,41 @@ export const newTeamMembers = [
         img : "https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
     }
 ]
+
+export const latestTransactions = [
+    {
+        customer : "Olivia Bailey",
+        img : "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        date : "3 March 2022",
+        amount : 119.99,
+        status : "Pending"
+    },
+    {
+        customer : "Olivia Bailey",
+        img : "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        date : "4 March 2022",
+        amount : 20.00,
+        status : "Declined" 
+    },
+    {
+        customer : "Olivia Bailey",
+        img : "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        date : "5 March 2022",
+        amount : 150.00,
+        status : "Approved"
+    },
+    {
+        customer : "Olivia Bailey",
+        img : "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        date : "1 March 2022",
+        amount : 90.50,
+        status : "Approved" 
+    },
+    {
+        customer : "Olivia Bailey",
+        img : "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        date : "3 March 2022",
+        amount : 80.99,
+        status : "Pending" 
+    }
+]
