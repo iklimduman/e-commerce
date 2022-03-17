18d493f52f94756bffa01cbae3598811867a9330
iklimduman <iklimdumann@gmail.com>
2022-03-17 14:55:11 +0300

    <Routes /> component used for page switching from react-router-dom

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import Topbar from "./Component/Topbar/Topbar";
 import Sidebar from './Component/Sidebar/Sidebar';
 import Home from './Component/Pages/Home';
+import UserPage from './Component/Pages/UserPage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />}>
+            
+          </Route>
+          <Route exact path="/users" element={<UserPage />}>
+            
+          </Route>
+        </Routes>
+
       </div>
 
-    </div>
+    </Router>
   );
 }
 
---
+++ admin/src/Component/Pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import react from "react" ;
+import "./UserPage.css" ;
+
+const UserPage = () => {
+    return(
+        <div className="userList">
+            users
+        </div>
+    )
+}
+
+export default UserPage ;
