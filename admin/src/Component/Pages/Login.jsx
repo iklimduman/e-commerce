a7afce22aed61bece1fac9c061f0b4c9d0ecc6e3
iklimduman <iklimdumann@gmail.com>
2022-03-30 11:58:15 +0300

    read token from persist root from browsers local storage.

--- admin/src/Component/Pages/Login.jsx
+++ admin/src/Component/Pages/Login.jsx
@@ -1,45 +1,61 @@
-import react , { useState} from "react" ;
-import { useDispatch } from "react-redux" ;
-import { login } from "../../Redux/apiCalls" ;
+import react, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../Redux/apiCalls";
 
 export const Login = () => {
 
     const [user, setUser] = useState({
-        username : "",
-        password : ""
-    }) ;
-    
-    const dispatch = useDispatch() ;
+        username: "",
+        password: ""
+    });
+
+    const dispatch = useDispatch();
 
     const handleChange = (e) => {
-        switch(e.target.name){
-            case "username-input" :
+        switch (e.target.name) {
+            case "username-input":
                 setUser({
                     ...user,
-                    username : e.target.value
+                    username: e.target.value
                 })
                 break;
-            case "password-input" :
+            case "password-input":
                 setUser({
                     ...user,
-                    password : e.target.value
+                    password: e.target.value
                 })
-                break ;
-            
-            default :
+                break;
+
+            default:
         }
     }
-    
+
     const handleLogin = (e) => {
         e.preventDefault();
-        login(dispatch,user) ;
+        login(dispatch, user);
     }
 
     return (
-        <div>
-            <input type="text" name="username-input" placeholder="username" onChange={e => handleChange(e)}/>
-            <input type="password" name="password-input" placeholder="password" onChange={e => handleChange(e)}/>
-            <button onClick={handleLogin}>Login</button>
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100vw",
+            height: "100vh",
+        }}>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column"
+            }}>
+                <input style={{ width: "20vw", border: "1px solid #DFDEE0", height: "50px" }}
+                    type="text" name="username-input" placeholder="username" onChange={e => handleChange(e)} />
+                <input style={{ width: "20vw", border: "1px solid #DFDEE0", height: "50px" }}
+                    type="password" name="password-input" placeholder="password" onChange={e => handleChange(e)} />
+                <button style={{width : "50%"}}
+                onClick={handleLogin}>Login</button>
+            </div>
         </div>
     )
 }
--- admin/src/Redux/RequestMethods.js
+++ admin/src/Redux/RequestMethods.js
@@ -4,7 +4,10 @@ const BASE_URL = "http://localhost:5000/api/" ;
 
 // after i will read this token on login auth method, but for now i am using this temporary token
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDY5YmNiY2QwZDA2NTgxN2E0MmI3ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NjMxODUyMSwiZXhwIjoxNjQ2NTc3NzIxfQ.ooAE1WJBQQmhJtLnW9UgMbjBeANE1L2S729uJsLsuP4" ;
+const TOKEN = (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)).currentUser.accesToken ;
+
+console.log( TOKEN ) ;
+
 
 export const publicRequest = axios.create({
     baseURL : BASE_URL,
