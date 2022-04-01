5e186d1a32750a395c08e5f997b26465a6a60bb9
iklimduman <iklimdumann@gmail.com>
2022-04-01 13:22:49 +0300

    fetch random user from different api

--- ecommerce/src/Components/Comments.jsx
+++ ecommerce/src/Components/Comments.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import React from "react";
 
-const BASE_URI = "https://random-data-api.com/api/";
+const BASE_URI = "https://randomuser.me/api/";
 
 async function getRandomUser() {
     try {
-        const data = await axios.get(BASE_URI + "users/random_user?size=4");
-        return data;
+        const data = await axios.get(BASE_URI + "?format=json");
+        return data ;
     }
     catch (err) {
         console.log(err);
@@ -14,22 +14,21 @@ async function getRandomUser() {
 }
 
 export const Comments = () => {
-    const randomData = getRandomUser();
+    const randomData = getRandomUser() ;
+    console.log(randomData) ;
 
-    var userArr = [] ;
-    const currentYear = new Date().getFullYear() ;
+    var userArr = [];
 
     Promise.resolve(randomData).then((value) => {
-        value.data.forEach(element => {
-            var randomUser = {
-                firstName : element.first_name , 
-                lastName : element.last_name , 
-                gender : element.gender , 
-                age : currentYear - parseInt(element.date_of_birth.split("-")[0])
-            }
-            userArr.push(randomUser) ;
-        });
-    });
+        var tempUser = {
+            firstName : value.data.results[0].name.first ,
+            lastName : value.data.results[0].name.last ,
+            gender : value.data.results[0].gender , 
+            age : value.data.results[0].dob.age ,
+            img : value.data.results[0].picture.medium
+        }
+        userArr.push(tempUser) ;
+    })
 
     console.log(userArr) ;
 
